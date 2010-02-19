3564b3892e68367ec28fc2346a6864ca09a3a600
Mark Pitchless <markpitchless@gmail.com>
2010-02-19 09:20:37 -0500

    Fix synopsis.pl example for current Log::Any usage.

--- examples/synopsis.pl
+++ examples/synopsis.pl
@@ -3,8 +3,8 @@
 use strict;
 use warnings;
 use Log::Any qw($log);
-use Log::Any::Adapter::Term;
-Log::Any->set_adapter( 'Term', level => 'info' );
+use Log::Any::Adapter;
+Log::Any::Adapter->set( 'Term', level => 'info' );
 
 $log->info("Hello");
 $log->notice("Lots happening");
